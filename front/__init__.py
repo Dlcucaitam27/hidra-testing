f596f50d0e561b9bfe5bcded414b727d0d38f9f8
David Leonardo Cucaita <165862850+Dlcucaitam27@users.noreply.github.com>
2026-09-20 22:57:14 -0500

    Delete front directory

--- front/__init__.py
+++
@@ -1,9 +0,0 @@
-from .styles import inyectar_css_selector
-from .pages import (
-    login_page,
-    pantalla_cambiar_password,
-    pantalla_selector,
-    formulario_casos,
-    panel_visualizacion,
-    panel_gestion_usuarios,
-)
