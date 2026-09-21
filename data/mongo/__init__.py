d3db4fa4442b0150490fbd751c1bb4d43a5c4cd1
David Leonardo Cucaita <165862850+Dlcucaitam27@users.noreply.github.com>
2026-09-20 22:57:04 -0500

    Delete data directory

--- data/mongo/__init__.py
+++
@@ -1,8 +0,0 @@
-from .usuarios_repo import (
-    conectar_sheet_usuarios,
-    obtener_usuario,
-    actualizar_password,
-    crear_usuario,
-    listar_usuarios,
-)
-from .casos_repo import conectar_sheet_casos
